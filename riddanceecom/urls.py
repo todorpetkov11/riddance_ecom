a3748e2513678c81546850e2d315ece0b83f529e
todorpetkov11 <76042756+todorpetkov11@users.noreply.github.com>
2021-08-05 18:53:41 +0300

    Add files via upload

--- riddanceecom/urls.py
+++ riddanceecom/urls.py
@@ -13,9 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from riddanceecom import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+                  path('admin/', admin.site.urls),
+                  path('', include('common.urls')),
+                  path('', include('products.urls')),
+                  path('accounts/', include('accounts.urls')),
+                  path('', include('cart.urls'))
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'riddanceecom.views.handler404'
+handler500 = 'riddanceecom.views.handler500'
+handler403 = 'riddanceecom.views.handler403'
+handler400 = 'riddanceecom.views.handler400'
