7183810a9b5c03e19ab9ac7395115152e5d7fc80
todorpetkov11 <76042756+todorpetkov11@users.noreply.github.com>
2021-08-06 19:29:41 +0300

    Add files via upload

--- common/urls.py
+++ common/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from common.views import BrowseView, LandingView
 
 urlpatterns = [
-    path('browse/', BrowseView.as_view(), name='browse'),
-    path('', LandingView.as_view(), name='landing')
+    path('', BrowseView.as_view(), name='browse'),
+    path('about_us/', LandingView.as_view(), name='landing')
 ]
