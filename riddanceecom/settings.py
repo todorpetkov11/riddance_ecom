305ffb88180cdd1f25c66ca340f89fa4641d711c
todorpetkov11 <76042756+todorpetkov11@users.noreply.github.com>
2021-08-08 12:10:09 +0300

    Update settings.py

--- riddanceecom/settings.py
+++ riddanceecom/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-224ks9_=$^rkm5s9!$!o=je=-3k+z@=$eb86=98#z*3pr==120'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'riddanceecom',
         'USER': 'postgres',
-        'PASSWORD': 'QWE1234',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
